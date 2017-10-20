c45b82e80408c7057d0159eb8792687c6877cdd9
Tara Grimm <tararochegrimm@gmail.com>
2017-10-20 11:21:50 -0400

    Lesson 3
    Learning about props.

--- views/index.jsx
+++ views/index.jsx
@@ -9,15 +9,31 @@ export default class TodoBox extends React.Component {
                 <TodoForm />
             </div>
         );
-    }
-}
+    };
+};
 
 class TodoList extends React.Component {
     render() {
         return (
             <div className="todoList">
-                I am a TodoList.
+                <table style={{ border: "2px solid black;" }}>
+                    <tbody>
+                        <Todo title="Shopping">Milk</Todo>
+                        <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
             </div>
+        );
+    };
+};
+
+class Todo extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td style={{ border: "1px solid black;" }}>{this.props.title}</td>
+                <td style={{ border: "1px solid black;" }}>{this.props.children}</td>
+            </tr>
         );
     };
 };
