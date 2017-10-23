ec100131561953934ce5231d47ca95538f4e3f7f
Tara Grimm <tararochegrimm@gmail.com>
2017-10-23 15:00:07 -0400

    Lesson 10
    Learning more about state.

--- views/index.jsx
+++ views/index.jsx
@@ -14,14 +14,54 @@ export default class TodoBox extends React.Component {
 };
 
 class TodoList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: this.props.data,
+            titleValue: "",
+            detailValue: ""
+        };
+        this.changeTitle = this.changeTitle.bind(this);
+        this.changeDetail = this.changeDetail.bind(this);
+        this.addTodo = this.addTodo.bind(this);
+    };
+
+    changeTitle(e) {
+        this.setState({ titleValue: e.target.value });
+    };
+
+    changeDetail(e) {
+        this.setState({ detailValue: e.target.value });
+    };
+
+    addTodo(e) {
+        let newTodo = this.state.data.push(
+            {
+                title: this.state.titleValue,
+                detail: this.state.detailValue
+            }
+        )
+
+        this.setState({
+            data: newTodo,
+            titleValue: "",
+            detailValue: ""
+        });
+    };
+
     render() {
         let todo = this.props.data.map(((obj) => {
             return (
                 <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>
-            )
+            );
         }));
         return (
             <div className="todoList">
+                <div>
+                    Title:<input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
+                    Detail:<input type="text" value={this.state.detailValue} onChange={this.changeDetail} />
+                    <button onClick={this.addTodo}>Add</button>
+                </div>
                 <table style={style.todoList}>
                     <tbody>
                         {todo}
